32080f184f535fd59cd61e4c587dd6ea2bccd2b1
ColinSz <kenavt@gmail.com>
2014-09-07 03:19:22 -0400

    Bug fixes for gears 2 and beyond

--- src/com/cszechy/backseat_driver/ShiftingLogic.java
+++ src/com/cszechy/backseat_driver/ShiftingLogic.java
@@ -2,35 +2,36 @@ package com.cszechy.backseat_driver;
 
 import android.util.Log;
 
+import com.openxc.measurements.IgnitionStatus;
 import com.openxc.measurements.TransmissionGearPosition;
 
 
 public class ShiftingLogic {
 	private CarDataPacket cardata;
 	private boolean brake, accelerator, clutch;
-	private boolean[] shifter;
+	private int shifter;
 	private int clutchThreshold, rollingSpeed, maxSuggestRPM, minSuggestRPM;
 	private int prevGear;
+	private IgnitionStatus.IgnitionPosition key = IgnitionStatus.IgnitionPosition.IGN_OFF;
 	private String nextAction;
+	private boolean shiftingUp = false, shiftingDown = false;
 	
-	public ShiftingLogic(CarDataPacket cardata) {
+	public ShiftingLogic(CarDataPacket cardata, int prevGear, boolean shiftingUp,
+			boolean shiftingDown) {
 		this.cardata = cardata;
 		this.brake = false;
-		this.shifter = new boolean[7];
-		this.shifter[0] = false;
-		this.shifter[1] = false;
-		this.shifter[2] = false;
-		this.shifter[3] = false;
-		this.shifter[4] = false;
-		this.shifter[5] = false;
-		this.shifter[6] = false;
+		this.shifter = prevGear;
+		this.prevGear = prevGear;
 		this.clutch = false;
 		this.accelerator = false;
 		this.clutchThreshold = 25;
-		this.maxSuggestRPM = 2100;
-		this.minSuggestRPM = 600;
-		this.rollingSpeed = 5;
+		this.maxSuggestRPM = 900;
+		this.minSuggestRPM = 500;
+		this.rollingSpeed = 3;
 		this.nextAction = "";
+		this.key = cardata.getIgnition();
+		this.shiftingUp = shiftingUp;
+		this.shiftingDown = shiftingDown;
 	}
 	
 	public void setClutchThreshold(int threshold) {
@@ -54,24 +55,20 @@ public class ShiftingLogic {
 	}
 	
 	private void accelerator(boolean val) {
-		if (this.cardata.getAccelPedalPos()!=0)
+		/*if (this.cardata.getAccelPedalPos()!=0)
 			this.accelerator = val;
 		else
-			this.accelerator = !val;
+			this.accelerator = !val;*/
+		this.accelerator = val;
 	}
 	
 	private void shifter(int val) {
-		this.shifter[0] = val==0;
-		this.shifter[1] = val==1;// && this.cardata.getGear()==TransmissionGearPosition.GearPosition.FIRST;
-		this.shifter[2] = val==2;// && this.cardata.getGear()==TransmissionGearPosition.GearPosition.SECOND;
-		this.shifter[3] = val==3;// && this.cardata.getGear()==TransmissionGearPosition.GearPosition.THIRD;
-		this.shifter[4] = val==4;// && this.cardata.getGear()==TransmissionGearPosition.GearPosition.FOURTH;
-		this.shifter[5] = val==5;// && this.cardata.getGear()==TransmissionGearPosition.GearPosition.FIFTH;
-		this.shifter[6] = val==6;// && this.cardata.getGear()==TransmissionGearPosition.GearPosition.SIXTH;
+		shifter = val;
 	}
 	
 	private void brake(boolean val) {
-		this.brake = this.cardata.getBrake()==val;
+		//this.brake = this.cardata.getBrake()==val;
+		this.brake = val;
 	}
 	
 	private int convertEnum(TransmissionGearPosition.GearPosition gearPos) {
@@ -93,99 +90,141 @@ public class ShiftingLogic {
 	
 	public void determineAction() {
 		//If engine is running
-		if (this.cardata.getRPM()>10){
+		if (key == IgnitionStatus.IgnitionPosition.IGN_RUN){
 			//If parking brake applied
 			if (this.cardata.getParkStatus()) {
-				//parkbrake off
-			} else {	//if parking brake not applied
+				clutch(true);
+				accelerator(false);
+				brake(true);
+				shifter(1);
+				this.nextAction="Release the parking brake!";
+			}
+			//if parking brake not applied
+			else {	
 				//if vehicle is moving
 				if (this.cardata.getMPH()>=this.rollingSpeed) {
 					//if it's time to upshift
 					if (this.cardata.getRPM()>=this.maxSuggestRPM) {
 						//if clutch is pressed in
 						if (this.cardata.getClutchPedalPos()) {
-							//if user has upshifted already
-							if (convertEnum(this.cardata.getGear())>this.prevGear) {
-								clutch(false);
-								accelerator(false);
-								this.nextAction="Release the clutch";
-							} else {	//if user has not upshifted already
-								clutch(true);
-								accelerator(false);
-								shifter(this.prevGear+1);
-								this.nextAction="Shift to the next gear";
-							}							
-						} else {	//if clutch is not pressed in 
+							//tell user to shift up
+							if(!shiftingUp)
+								shifter(shifter+1);
+							this.nextAction = ("Move into the next gear and release the clutch");
+							clutch(true);
+							accelerator(false);
+							brake(false);
+							shiftingUp = true;
+						}	//if just finished pushing in clutch, finish shifting up
+						/*else if (shiftingUp) {
+							shiftingUp = false;
 							clutch(false);
-							accelerator(true);
-							this.nextAction="Begin to accelerate";
+							accelerator(false);
+							brake(false);
+							this.nextAction = ("Begin accelerating");
+						}		*/
+						//if clutch is not pressed in
+						else { 
+							shiftingUp = false;
+							clutch(true);
+							accelerator(false);
+							brake(false);
+							this.nextAction="Push the clutch in to begin upshifting";
 						}
 					//if close to stalling
 					} else if (this.cardata.getRPM()<=this.minSuggestRPM) {
 						//if clutch is pressed in
 						if (this.cardata.getClutchPedalPos()) {
 							//if user has downshifted already
-							if (convertEnum(this.cardata.getGear())<this.prevGear) {
+							//if (shiftingDown) {
 								clutch(false);
 								accelerator(true);
-								this.nextAction="Release the clutch";
-							} else {	//if user has not downshifted already
-								clutch(true);
-								accelerator(false);
-								shifter(this.prevGear-1);
-								this.nextAction="Shift to the previous gear";
-							}							
-						} else {	//if clutch is not pressed in 
-							clutch(false);
+								brake(false);
+								shiftingDown = true;
+								this.nextAction="Move the shifter down, then release the clutch";
+							}					
+						//if clutch is not pressed in
+						else {	 
+							if(!shiftingDown)
+								shifter(shifter-1);
+							shiftingDown = true;
+							clutch(true);
+							brake(false);
 							accelerator(true);
-							this.nextAction = "Begin to accelerate";
+							this.nextAction = "Press in the clutch to begin downshifting";
 						}
-					} else { //if inbetween high and low RPM ranges
-						prevGear = convertEnum(this.cardata.getGear());
-						this.nextAction = "Maintain current status";
 					}
-				} else { //If vehicle is not moving
-						//if vehicle is in first
-					if (this.cardata.getGear()==TransmissionGearPosition.GearPosition.FIRST) {
-						//If clutch is pushed in, tell the driver to do this
-						if (this.cardata.getClutchPedalPos()) {
-							clutch(false);
-							brake(false);
-							accelerator(false);
-							shifter(1);
-							this.nextAction = "Slowly release the clutch";
-							//if clutch is not in, instruct driver to do this
-						} else {
-							clutch(false);
-							brake(false);
-							accelerator(true);
-							shifter(1);
-							this.nextAction = "Completely release the clutch and accelerate";
-						}
-						//If we're not in first
-					} else {
-						clutch(true);
+					//if inbetween high and low RPM ranges
+					else {
+						clutch(false);
+						accelerator(true);
+						brake(false);
+						shiftingDown = false;
+						shiftingUp = false;
+						this.nextAction = "Maintain current speed";
+					}
+				}	
+				//if vehicle is stationary 
+				else { 
+					//If clutch is pushed in
+					if (this.cardata.getClutchPedalPos()) {
+						clutch(false);
+						brake(false);
+						accelerator(true);
 						shifter(1);
+						this.nextAction = "Shift into first, and begin to accelerate " +
+								"while coming off the clutch";
+					} 
+					//clutch not in, brake is
+					else if (brake) {
+						clutch(true);
+						brake(false);
 						accelerator(false);
-						this.nextAction = "Shift into first gear";
+						shifter(0);
+						this.nextAction = "Press in the clutch.";
+					}
+					//clutch and brake are not pushed in 
+					else if (!brake){	
+						clutch(true);
+						brake(true);
+						accelerator(false);
+						shifter(0);
+						this.nextAction = "Press in the clutch and the brake.";
 					}
 				}
 			}
-		} else { //if engine is not running, begin running procedures
+		} else { //if engine is not running, START THE FUCKING CAR
 			brake(true);
 			clutch(true);
 			shifter(0);
 			accelerator(false);
-			this.nextAction = "Engage the clutch while in neutral";
-			//turn the key
+			this.nextAction = "Press the button, push to start.";
 		}
 	}
 	
-	public String getNextDirection() { return this.nextAction; }
-	public boolean[] getShifter() { return this.shifter; }
-	public boolean getClutch() { return this.clutch; }
-	public boolean getAccelerator() { return this.accelerator; }
-	public boolean getBrake() { return this.brake; }
+	public String getNextDirection() { 
+		return this.nextAction; 
+		}
+	public int getShifter() {
+		return shifter;
+	}
+	public boolean getClutch() {
+		return this.clutch; 
+	}
+	public boolean getAccelerator() { 
+		return this.accelerator; 
+	}
+	public boolean getBrake() { 
+		return this.brake; 
+	}
+	
+	public boolean getShiftUp() {
+		return shiftingUp;
+	}
+	
+	public boolean getShiftDown() {
+		return shiftingDown;
+	}
 	
 	public void printParams() {
 		Log.d("brake", "" + cardata.getBrake());
@@ -194,5 +233,8 @@ public class ShiftingLogic {
 		Log.d("engine speed", "" + cardata.getRPM());
 		Log.d("nextAction", "" + nextAction);
 		Log.d("road speed", "" + cardata.getMPH());
+		Log.d("parking brake", "" + cardata.getParkStatus());
+		Log.d("ignition", "" + key.toString());
+		Log.d("shifter", "" + shifter);
 	}
 }
